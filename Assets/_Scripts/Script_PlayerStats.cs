bcd12b273b0a993fdc5d5bc32f97c430e2e1cc5c
DepressedCookie <vipc.1224@live.ca>
2020-11-27 20:43:16 -0500

    Added health system

---
+++ Assets/_Scripts/Script_PlayerStats.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_PlayerStats : MonoBehaviour
+{
+    [SerializeField] float Health = 100.0f;
+    [SerializeField] bool IsAlive = true;
+    public Transform HPBar;
+
+    void CheckAlive()
+    {
+        if(Health <= 0)
+        {
+            Health = 0;
+            IsAlive = false;
+        }
+    }
+
+    void ApplyDamage(float damage)
+    {
+        Health -= damage;
+        CheckAlive();
+        UpdateHPBar();
+    }
+
+    void UpdateHPBar()
+    {
+        Vector2 sc = HPBar.localScale;
+        sc.x = Health / 100.0f;
+        HPBar.localScale = sc;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Enemy")
+        {
+            ApplyDamage(15.0f);
+        }
+
+        if (collision.tag == "Bullet")
+        {
+            ApplyDamage(5.0f);
+        }
+    }
+}
